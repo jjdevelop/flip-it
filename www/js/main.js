a2c0cabe89b6d54b5cf1a57b932967a4ff3d3d21
JJ_Develop_Johnny <jjdevelopsearch@gmail.com>
2016-07-31 11:23:46 -0700

    Added sprite

--- www/js/main.js
+++ www/js/main.js
@@ -1,16 +1,25 @@
-(function () {
+(function() {
     'use strict';
-    
+
     var game = new Phaser.Game(window.innerWidth * window.devicePixelRatio, window.innerHeight * window.devicePixelRatio, Phaser.CANVAS, 'gameDiv');
 
     var mainState = {
 
         preload: function() {
             game.stage.backgroundColor = '#71c5cf';
+
+            game.load.spritesheet('square', 'img/sprites/square.png', 128, 128);
         },
 
         create: function() {
             game.physics.startSystem(Phaser.Physics.ARCADE);
+
+            var square = game.add.sprite(0, 0, 'square');
+
+            game.physics.arcade.enable(square);
+
+            square.animations.add('left', [0], 10, true);
+            square.animations.add('right', [1], 10, true);
         },
 
         restartGame: function() {
