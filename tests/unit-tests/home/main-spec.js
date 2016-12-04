a5f12ed15136d187715999b30ebca80eccb2be14
Johnny Su <johnny.su@expeditors.com>
2016-12-04 10:44:56 -0800

    Empty test spec for main.js

---
+++ tests/unit-tests/home/main-spec.js
@@ -0,0 +1,21 @@
+'use strict';
+
+describe('Phaser Game Functions', function() {
+
+     // beforeEach(module('flipit'));
+
+    describe('Toggle Squares', function() {
+    	var modifyingSquares;
+
+        beforeEach(inject(function () {
+        	// Create Squares
+            modifyingSquares = [];
+        }));
+
+        it('should have the expected type properties', function() {
+            expect(modifyingSquares).toBeDefined();
+        });
+
+    });
+
+});
